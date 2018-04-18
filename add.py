0e902963a8afdfc671d9bc649d47e6a1f96308fa
user <user@fisat>
2018-04-18 04:51:25 -0400

    created a function to add 3 nos

--- add.py
+++ add.py
@@ -1,6 +1,12 @@
-def add2(n1, n2):
+def add2(n1,n2):
 			s = n1 + n2
 			return s
 
-print add2(3, 4)
+def add3(n1,n2,n3):
+			s = n1 + n2 +n3
+			return s
+
+
+print add2(3,4)
+print add3(3,4,5)
 	
